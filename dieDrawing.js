02d3bcd828d598a672cfb67ee92285ddbc324e63
Jack Shea <jack20shea20@gmail.com>
2022-01-18 05:49:55 -0500

    started the drawing function for the die faces

---
+++ dieDrawing.js
@@ -0,0 +1,29 @@
+function draw1(die){
+    let dotX;
+    let dotY;
+    dotX = Dice[die].x + 0.5*Dice[die].width;
+    dotY = Dice[die].y + 0.5*Dice[die].height;
+    ctx.beginPath();
+    fullCircle(dotX,dotY,Dice[die].dotRad);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function draw2(die){
+    let dotX = Dice[die].x + 3*Dice[die].dotRad;
+    let dotY = Dice[die].y + 3*Dice[die].dotRad;
+    ctx.beginPath();
+    fullCircle(dotX,dotY,Dice[die].dotRad); 
+    dotX = Dice[die].x+Dice[die].width-3*Dice[die].dotRad;
+    dotY = Dice[die].y+Dice[die].height-3*Dice[die].dotRad;
+    fullCircle(dotX,dotY,Dice[die].dotRad); 
+    ctx.closePath();
+    ctx.fill();
+}
+
+
+setInterval(dieThrow, 1000);
+
+
+
+
