454d5b8292d571b8ed904fd9c1621b2074e32f80
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-01-18 15:22:54 -0500

    retart

--- dieDrawing.js
+++ dieDrawing.js
@@ -1,25 +1,4 @@
-function draw1(die){
-    let dotX;
-    let dotY;
-    dotX = Dice[die].x + 0.5*Dice[die].width;
-    dotY = Dice[die].y + 0.5*Dice[die].height;
-    ctx.beginPath();
-    fullCircle(dotX,dotY,Dice[die].dotRad);
-    ctx.closePath();
-    ctx.fill();
-}
 
-function draw2(die){
-    let dotX = Dice[die].x + 5*Dice[die].dotRad;
-    let dotY = Dice[die].y + 5*Dice[die].dotRad;
-    ctx.beginPath();
-    fullCircle(dotX,dotY,Dice[die].dotRad); 
-    dotX = Dice[die].x+Dice[die].width-5*Dice[die].dotRad;
-    dotY = Dice[die].y+Dice[die].height-5*Dice[die].dotRad;
-    fullCircle(dotX,dotY,Dice[die].dotRad); 
-    ctx.closePath();
-    ctx.fill();
-}
 
 
 
