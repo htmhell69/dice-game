4b8d07479c2325c1d76fd23d7f4c8ffc39611160
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-01-18 16:12:51 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,4 +1,19 @@
+var ctx = document.getElementById("canvas").getContext('2d');
 var canvasWidth = 400;
 var canvasHeight = 300;
+var dice = [];
+function createDie(x, y, height, width, dotSize){
+  let die = {
+    x: x,
+    y: y,
+    height: height,
+    width: width,
+    dotSize: dotSize
+  }
+  dice.push(die);
+}
+
+createDie(50,50,50,50,50);
+alert (dice[0].x);
 
 
