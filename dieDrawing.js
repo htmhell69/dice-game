1cb594f892651272bb3796097c38ebbf6230bc75
htmhell69 <96142014+htmhell69@users.noreply.github.com>
2022-01-21 12:10:00 -0500

    Update dieDrawing.js

--- dieDrawing.js
+++ dieDrawing.js
@@ -3,6 +3,8 @@ function draw1(die){
   let dotX = die.x + .5*die.width;
   let dotY = die.y + .5*die.height;
   ctx.arc(dotX,dotY,die.dotSize,0,Math.PI*2,true);
+  ctx.closePath();
+  ctx.fill();
 }
 
 
