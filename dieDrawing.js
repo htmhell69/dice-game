64b4dc665042784d2c44590a5bd3f4a1fc664c2c
Jack Shea <jack20shea20@gmail.com>
2022-01-23 20:38:04 -0500

    set up the basis for the draw dot function

--- dieDrawing.js
+++ dieDrawing.js
@@ -1,10 +1,21 @@
 function draw1(die){
-  let ctx = document.getElementById("canvas").getContext('2d');
-  alert(1);
   ctx.beginPath();
   let dotX = die.x + .5*die.width;
   let dotY = die.y + .5*die.height;
   ctx.arc(dotX,dotY,die.dotSize,0,Math.PI*2,true);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function draw2(die){
+  alert(2);
+  ctx.beginPath();
+  let dotX = die.x + (3*die.dotSize);
+  let dotY = die.y + (3*die.dotSize);
+  ctx.arc(dotX,dotY,die.dotSize,0,Math.PI*2);
+  dotX = die.x + die.width - (3*die.dotSize);
+  dotY = die.y + die.height - (3*die.dotSize);
+  ctx.arc(dotX,dotY,die.dotSize,0,Math.PI*2);
   ctx.closePath();
   ctx.fill();
 }
--- setup.js
+++ setup.js
@@ -4,7 +4,8 @@ var canvasHeight = 300;
 var Dice = [];
 //this variable holds the result of the die roll
 var ch;
-
+//canvas
+var ctx = document.getElementById("canvas").getContext('2d');
 //creating functions
 
 //this function creates a die object and puts it in the Dice array
@@ -23,14 +24,13 @@ function createDie(x, y, height, width, dotSize){
 //this function gets a random number 1-6 then calls the draw function. It does this for each die object in the Dice array
 function rollDie(){
   for(let i=0; i<Dice.length; i++){
-    ch = 1;
+    ch = Math.random();
     drawDots(ch, Dice[i]);
   }
 }
 
 //this function is responsable for drawing the dots on a die it takes a number so amount of dots and the die so it knows where to draw it.
 function drawDots(num, die){
-  let ctx = document.getElementById("canvas").getContext('2d');
   ctx.lineWidth = 5;
   ctx.clearRect(die.x,die.y,die.width,die.height);
   ctx.strokeRect(die.x,die.y,die.width,die.height);
@@ -62,7 +62,7 @@ function drawDots(num, die){
 }
 
 
-createDie(50,50,100,100,5);
+createDie(50,50,100,100,10);
   
 
 
